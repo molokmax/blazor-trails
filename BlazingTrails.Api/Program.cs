934965189285e4d6b97e32d3be6a24f2efbc5d3f
Maksim Molokanov <maks.molokanov@gmail.com>
2023-06-01 20:40:33 +0300

    chapter 5

---
+++ BlazingTrails.Api/Program.cs
@@ -0,0 +1,46 @@
+﻿using BlazingTrails.Api.Persistence;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace BlazingTrails.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("BlazingTrailsContext");
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                builder.Services.AddDbContext<BlazingTrailsContext>(options =>
+                    options.UseSqlite(connectionString));
+            }
+
+            builder.Services
+                .AddControllers();
+            builder.Services
+                .AddFluentValidationAutoValidation()
+                .AddValidatorsFromAssembly(Assembly.Load("BlazingTrails.Shared"));
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+                app.UseWebAssemblyDebugging();
+
+            app.UseHttpsRedirection();
+
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapControllers();
+            app.MapFallbackToFile("index.html");
+
+            app.Run();
+        }
+    }
+}
--- BlazingTrails.Client/Client/Program.cs
+++ BlazingTrails.Client/Client/Program.cs
@@ -1,20 +1,21 @@
 using BlazingTrails.Client;
+using MediatR;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace BlazingTrails.Client
+namespace BlazingTrails.Client;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddMediatR(typeof(Program).Assembly);
 
-            await builder.Build().RunAsync();
-        }
+        await builder.Build().RunAsync();
     }
 }
